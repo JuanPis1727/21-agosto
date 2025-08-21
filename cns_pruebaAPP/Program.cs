c5170f9f347f68f13b9d4265716f7994ad3fa51a
Sala K 301 <salak301@salasfr.itm.edu.co>
2025-08-21 09:42:19 -0500

    Primer proyecto editado

--- cns_pruebaAPP/Program.cs
+++ cns_pruebaAPP/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
 Console.WriteLine("Hello, World!");
+
+
+public class Personas{
+    public int id;
+    public String? Nombre;
+    public decimal Estatura;
+    public bool vive;
+    public DateTime Fecha;
+}
